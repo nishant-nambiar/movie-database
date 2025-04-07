1e6a919493191fdbec16f742b2b5032f54dd7629
nishant-nambiar <nishantnambiar1823@gmail.com>
2025-04-07 11:02:05 -0400

    Code Files

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "6bb21e2ba6e42c8e0064fd81e227f8df";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
